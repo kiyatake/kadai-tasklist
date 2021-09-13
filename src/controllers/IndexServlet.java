c888e2496cd0d448c4600ba0122b0c49b4b12ff4
tomoki.kiyatake <tomokik0298@gmail.com>
2021-09-13 11:40:55 +0900

    Add database files

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,9 +35,13 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Tasklist> Tasks = em.createNamedQuery("getAllTasks", Tasklist.class).getResultList();
-        response.getWriter().append(Integer.valueOf(Tasks.size()).toString());
+        List<Tasklist> tasks = em.createNamedQuery("getAllTasks", Tasklist.class).getResultList();
 
         em.close();
+
+        request.setAttribute("tasks", tasks);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB=INF/views/tasks/index.jsp");
+        rd.forward(request, response);
     }
 }
